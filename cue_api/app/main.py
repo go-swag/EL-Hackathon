eae6a55bd6a4f80ed2eee5970f19340321c18e66
Conner Murphy <c.murphy@goswag.com>
2025-02-22 16:55:23 +0000

    Allowing wildcard on CORS fastAPI middleware (#4)

--- cue_api/app/main.py
+++ cue_api/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Body
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import StreamingResponse
 import time
 import os
@@ -7,6 +8,13 @@ from dotenv import load_dotenv
 load_dotenv()
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/submit-text")
 def submit_text(text: str = Body(..., embed=True)):
