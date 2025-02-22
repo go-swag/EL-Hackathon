780835c1c052dbc162783fbba9f7a451b4c9e303
Conner Murphy <c.murphy@goswag.com>
2025-02-22 23:45:18 +0000

    width fix

--- cue_ui/components/shell/AppPrimaryView.tsx
+++ cue_ui/components/shell/AppPrimaryView.tsx
@@ -2,7 +2,7 @@ const AppPrimaryView: React.FC<{
   children?: React.ReactNode;
 }> = ({ children }): React.JSX.Element => {
   return (
-    <div className="grow max-w-[calc(100%-30%)] border-r border-r-white/10">
+    <div className="grow w-full max-w-[calc(100%-30%)] border-r border-r-white/10">
       <div className="py-6 h-full">{children}</div>
     </div>
   );
--- cue_ui/components/shell/AppSideBar.tsx
+++ cue_ui/components/shell/AppSideBar.tsx
@@ -6,7 +6,7 @@ interface SnippetsPanelProps {
 
 const AppSideBar = ({ title }: SnippetsPanelProps) => {
   return (
-    <div className="h-full bg-black/20 min-w-[30%] overflow-y-auto">
+    <div className="h-full bg-black/20 max-w-[30%] overflow-y-auto">
       <div className="p-4 border-b border-white/10">
         <h2 className="text-2xl font-semibold text-white">{title}</h2>
       </div>
